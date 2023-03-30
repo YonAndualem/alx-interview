d8b852d2722568b1ea9953054436eb93e8a4b147
Yonas Andualem <104837049+Yonas1472@users.noreply.github.com>
2023-03-30 23:35:36 +0300

    new

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3
-"""Lockbox Challange"""
+'''LockBoxes Challenge'''
 
 
 def canUnlockAll(boxes):
-    """
-    Determines if all the boxes can be opened or not
-        Returns:
+    '''determines if all the boxes can be opened or not
+    Returns:
         True: all boxes can be opened
         False: not all boxes can be opened
-    """
-    n = len(boxes)
-    checked = set([0])
-    unchecked = set(boxes[0]).difference(set([0]))
+    '''
+    length = len(boxes)
+    keys = set()
+    opened_boxes = []
+    i = 0
 
-    while len(unchecked) > 0:
-        box = unchecked.pop()
-        if not box or box >= n or box < 0:
+    while i < length:
+        oldi = i
+        opened_boxes.append(i)
+        keys.update(boxes[i])
+        for key in keys:
+            if key != 0 and key < length and key not in opened_boxes:
+                i = key
+                break
+        if oldi != i:
             continue
-        if box not in checked:
-            unchecked = unchecked.union(boxes[box])
-            checked.add(box)
-    return n == len(checked)
+        else:
+            break
+
+    for i in range(length):
+        if i not in opened_boxes and i != 0:
+            return False
+    return True
     
