698f10c5128c02e8b73a58f561e2464ee8efe736
Yonas Andualem <104837049+Yonas1472@users.noreply.github.com>
2023-04-14 18:20:28 +0300

    pep validation

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,23 +3,27 @@
 import sys
 import logging
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.ERROR)
+
 
 if __name__ == "__main__":
     count = 0
-    totalSize = 0
+    total_size = 0
     codes = [200, 301, 400, 401, 403, 404, 405, 500]
     obj = {}
+
     try:
         for line in sys.stdin:
             lst = line.split(' ')
             if len(lst) < 2:
                 continue
+
             try:
                 count += 1
                 status = int(lst[-2])
                 size = int(lst[-1])
-                totalSize += size
+                total_size += size
+
                 if status in codes:
                     if status in obj:
                         obj[status] += 1
@@ -28,19 +32,19 @@ if __name__ == "__main__":
 
                 if count == 10:
                     count = 0
-                    logging.info('File size: {}'.format(totalSize))
+                    print('File size: {}'.format(total_size))
                     for el in sorted(obj.items()):
-                        logging.info('{}: {}'.format(el[0], el[1]))
+                        print('{}: {}'.format(el[0], el[1]))
 
             except ValueError:
                 logging.error('Wrong format: {}'.format(line.strip()))
                 continue
 
-        logging.info('File size: {}'.format(totalSize))
+        print('File size: {}'.format(total_size))
         for el in sorted(obj.items()):
-            logging.info('{}: {}'.format(el[0], el[1]))
+            print('{}: {}'.format(el[0], el[1]))
 
     except KeyboardInterrupt:
-        logging.info('File size: {}'.format(totalSize))
+        print('File size: {}'.format(total_size))
         for el in sorted(obj.items()):
-            logging.info('{}: {}'.format(el[0], el[1]))
+            print('{}: {}'.format(el[0], el[1]))
