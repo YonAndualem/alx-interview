ae4de5742c58ec1934a8027652d0c1be76d0774d
Yonas Andualem <104837049+Yonas1472@users.noreply.github.com>
2023-04-14 18:07:54 +0300

    generator

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,43 +1,43 @@
 #!/usr/bin/python3
-"""
-Module that performs log parsing from stdin
-"""
-
-import re
+''' script that reads stdin line by line and computes metrics '''
 import sys
-counter = 0
-file_size = 0
-statusC_counter = {200: 0, 301: 0, 400: 0,
-                   401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-
-
-def printCodes(dict, file_s):
-    """Prints the status code and the number of times they appear"""
-    print("File size: {}".format(file_s))
-    for key in sorted(dict.keys()):
-        if statusC_counter[key] != 0:
-            print("{}: {}".format(key, dict[key]))
-
 
 if __name__ == "__main__":
+    count = 0
+    totalSize = 0
+    codes = [200, 301, 400, 401, 403, 404, 405, 500]
+    obj = {}
     try:
         for line in sys.stdin:
-            split_string = re.split('- |"|"| " " ', str(line))
-            statusC_and_file_s = split_string[-1]
-            if counter != 0 and counter % 10 == 0:
-                printCodes(statusC_counter, file_size)
-            counter = counter + 1
+            lst = line.split(' ')
+            if len(lst) < 2:
+                continue
             try:
-                statusC = int(statusC_and_file_s.split()[0])
-                f_size = int(statusC_and_file_s.split()[1])
-                print("Status Code {} size {}".format(statusC, f_size))
-                if statusC in statusC_counter:
-                    statusC_counter[statusC] += 1
-                file_size = file_size + f_size
-            except:
-                pass
-        printCodes(statusC_counter, file_size)
+                count += 1
+                status = int(lst[-2])
+                size = int(lst[-1])
+                totalSize += size
+                if status in codes:
+                    if status in obj:
+                        obj[status] += 1
+                    else:
+                        obj[status] = 1
+
+                if count == 10:
+                    count = 0
+                    print('File size: {}'.format(totalSize))
+                    for el in sorted(obj.items()):
+                        print('{}: {}'.format(el[0], el[1]))
+
+            except ValueError:
+                continue
+
+        print('File size: {}'.format(totalSize))
+        for el in sorted(obj.items()):
+            print('{}: {}'.format(el[0], el[1]))
+
     except KeyboardInterrupt:
-        printCodes(statusC_counter, file_size)
-        raise
-        
+        print('File size: {}'.format(totalSize))
+        for el in sorted(obj.items()):
+            print('{}: {}'.format(el[0], el[1]))
+            
