a9a8ca48eeb298adf77465e7d7e7e7f38eabcaf3
Yonas Andualem <104837049+Yonas1472@users.noreply.github.com>
2023-04-14 06:29:44 +0300

    new try

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,32 +1,22 @@
 #!/usr/bin/python3
-
 """
 Module that performs log parsing from stdin
 """
 
 import re
 import sys
-
 counter = 0
 file_size = 0
-statusC_counter = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0
-}
+statusC_counter = {200: 0, 301: 0, 400: 0,
+                   401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
 
-def print_codes(dict, file_s):
+def printCodes(dict, file_s):
     """Prints the status code and the number of times they appear"""
-    print(f"File size: {file_s}")
+    print("File size: {}".format(file_s))
     for key in sorted(dict.keys()):
         if statusC_counter[key] != 0:
-            print(f"{key}: {dict[key]}")
+            print("{}: {}".format(key, dict[key]))
 
 
 if __name__ == "__main__":
@@ -35,16 +25,19 @@ if __name__ == "__main__":
             split_string = re.split('- |"|"| " " ', str(line))
             statusC_and_file_s = split_string[-1]
             if counter != 0 and counter % 10 == 0:
-                print_codes(statusC_counter, file_size)
-            counter += 1
+                printCodes(statusC_counter, file_size)
+            counter = counter + 1
             try:
-                statusC, f_size = map(int, statusC_and_file_s.split())
+                statusC = int(statusC_and_file_s.split()[0])
+                f_size = int(statusC_and_file_s.split()[1])
+                # print("Status Code {} size {}".format(statusC, f_size))
                 if statusC in statusC_counter:
                     statusC_counter[statusC] += 1
-                file_size += f_size
+                file_size = file_size + f_size
             except:
                 pass
-        print_codes(statusC_counter, file_size)
+        printCodes(statusC_counter, file_size)
     except KeyboardInterrupt:
-        print_codes(statusC_counter, file_size)
+        printCodes(statusC_counter, file_size)
         raise
+        
