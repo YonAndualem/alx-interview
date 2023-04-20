d9077bf5146ea5a0d6a29bdf0ad402736bd7b663
Yonas Andualem <104837049+Yonas1472@users.noreply.github.com>
2023-04-20 04:44:19 +0300

    Indentation modification

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,6 +1,8 @@
-#usr/bin/python3
+#!/usr/bin/python3
+""" UTF-8 Validation """
 
-def valid_utf8(data):
+
+def validUTF8(data):
     """
     Method that determines if a given data set represents a valid
     UTF-8 encoding.
@@ -12,7 +14,7 @@ def valid_utf8(data):
     need to handle the 8 least significant bits of each integer
     """
     # Variable for counting number of bytes in UTF-8 Character
-    num_bytes = 0
+    number_bytes = 0
 
     # Masks for checking if byte is valid (Starts with 10)
     mask1 = 1 << 7
@@ -22,22 +24,22 @@ def valid_utf8(data):
 
         mask_n_byte = 1 << 7
 
-        if num_bytes == 0:
+        if number_bytes == 0:
             # Count number of bytes the UTF-8 Character will have
             while mask_n_byte & i:
-                num_bytes += 1
+                number_bytes += 1
                 mask_n_byte = mask_n_byte >> 1
 
             # If number of bytes did not increase then it has 1 byte
             # which is the same we are counting so no need to check next bytes
             # for current character
-            if num_bytes == 0:
+            if number_bytes == 0:
                 continue
 
             # A character in UTF-8 can be 1 to 4 bytes long
-            # But 1 byte characters start in 0 so num_bytes should never
+            # But 1 byte characters start in 0 so number_bytes should never
             # be 1
-            if num_bytes == 1 or num_bytes > 4:
+            if number_bytes == 1 or number_bytes > 4:
                 return False
 
         else:
@@ -48,10 +50,10 @@ def valid_utf8(data):
 
         # If bytes of character are valid, then the count will decrease with
         # each byte until a new character starts
-        num_bytes -= 1
+        number_bytes -= 1
 
     # All characters were verified correctly with their proper byte count
-    if num_bytes == 0:
+    if number_bytes == 0:
         return True
 
     return False
